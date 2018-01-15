2193629c05334b8b1d4119e766800f05a404a948
Juan-Pablo Velez <jpvelez@spotify.com>
2018-01-15 18:20:15 -0500

    Debug WordCount1 with JavaConverters, proper process arg type

--- src/main/scala/WordCount1JPV.scala
+++ src/main/scala/WordCount1JPV.scala
@@ -1,10 +1,12 @@
-import WordCount0JPV.{SimpleDoFn, expected, input}
+import WordCount0.{SimpleDoFn, expected, input}
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.options.{PipelineOptions, PipelineOptionsFactory}
 import org.apache.beam.sdk.testing.PAssert
-import org.apache.beam.sdk.transforms.Create.Values
-import org.apache.beam.sdk.transforms.{Count, ParDo}
+import org.apache.beam.sdk.transforms.Create
+import org.apache.beam.sdk.transforms.{Count, DoFn, ParDo}
 import org.apache.beam.sdk.values.{KV, PCollection}
+
+import scala.collection.JavaConverters._
 
 object WordCount1JPV {
 
@@ -16,45 +18,37 @@ object WordCount1JPV {
     val options: PipelineOptions = PipelineOptionsFactory.create()
     val pipeline: Pipeline = Pipeline.create(options)
     def parallelize[A](input: Iterable[A]): PCollection[A] = {
-      pipeline.apply(new Values[A](input))
+      pipeline.apply(Create.of(input.asJava))
     }
     def close() = pipeline.run()
   }
 
-
   def main(args: Array[String]): Unit = {
     val sc = ScioContext(args)
-
-    val result: PCollection[String] = sc.parallelize(input)
+    val result: PCollection[String] = sc.parallelize(input.asScala)
       .apply(ParDo.of(new SimpleDoFn[String, String]("flatMap") {
-        override def process(c: ProcessContext) {
-          val line = c.element()
-          for (word <- line.toLowerCase().split(" ")) {
-            c.output(word)
-          }
+        override def process(c: DoFn[String,String]#ProcessContext) {
+          c.element().toLowerCase.split(" ").foreach(c.output(_))
         }
       }))
       .apply(ParDo.of(new SimpleDoFn[String, String]("filter") {
-        override def process(c: ProcessContext) {
+        override def process(c: DoFn[String, String]#ProcessContext) {
           val word = c.element()
-          if (!word.isEmpty()) {
-            c.output(word)
-          }
+          if (!word.isEmpty()) { c.output(word) }
         }
       }))
       .apply(Count.perElement())
-      .apply(ParDo.of(new SimpleDoFn[KV[String,Long], String]("map") {
-        override def process(c: ProcessContext) {
-          val kv: KV[String, Long] = c.element()
+      .apply(ParDo.of(new SimpleDoFn[KV[String, java.lang.Long], String]("map") {
+        override def process(c: DoFn[KV[String, java.lang.Long], String]#ProcessContext) {
+          val kv = c.element()
           val word = kv.getKey()
           val count = kv.getValue()
           c.output(word + " " + count)
         }
-      })
+      }))
 
     PAssert.that(result).containsInAnyOrder(expected)
 
     sc.close()
   }
-
 }
