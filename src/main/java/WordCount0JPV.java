bdeb90b5e8857efb1038a87a47e74577292dc32c
Juan-Pablo Velez <jpvelez@spotify.com>
2018-01-15 16:10:44 -0500

    Implement SimpleDoFn, refactor ParDo DoFns

--- src/main/java/WordCount0JPV.java
+++ src/main/java/WordCount0JPV.java
@@ -1,6 +1,5 @@
 import com.google.common.collect.Lists;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
@@ -39,18 +38,18 @@ public class WordCount0JPV {
 
         PCollection<String> result =  p
                 .apply(Create.of(input))
-                .apply(ParDo.of(new DoFn<String, String>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
+                .apply(ParDo.of(new SimpleDoFn<String, String>("flatMap") {
+                    @Override
+                    public void process(ProcessContext c) {
                         String line = c.element();
                         for (String word : line.toLowerCase().split(" ")) {
                             c.output(word);
                         }
                     }
                 }))
-                .apply(ParDo.of(new DoFn<String, String>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
+                .apply(ParDo.of(new SimpleDoFn<String, String>("filter") {
+                    @Override
+                    public void process(ProcessContext c) {
                         String word = c.element();
                         if (!word.isEmpty()) {
                             c.output(word);
@@ -58,9 +57,9 @@ public class WordCount0JPV {
                     }
                 }))
                 .apply(Count.perElement())
-                .apply(ParDo.of(new DoFn<KV<String,Long>, String>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
+                .apply(ParDo.of(new SimpleDoFn<KV<String,Long>, String>("map") {
+                    @Override
+                    public void process(ProcessContext c) {
                         KV<String, Long> kv = c.element();
                         String word = kv.getKey();
                         Long count = kv.getValue();
@@ -71,5 +70,38 @@ public class WordCount0JPV {
         PAssert.that(result).containsInAnyOrder(expected);
 
         p.run().waitUntilFinish();
+    }
+
+    public static abstract class SimpleDoFn<A, B> extends DoFn<A, B> {
+
+        public String name;
+
+        public SimpleDoFn(String name) { this.name = name; }
+
+        public abstract void process(DoFn<A, B>.ProcessContext c);
+
+        public void log(String step) {
+            System.out.println(
+                String.format("%s %s %s thread: %s", name, step, this, Thread.currentThread().getId()));
+        }
+
+        @Setup
+        public void setup() { this.log("Setup"); }
+
+        @StartBundle
+        public void startBundle() { this.log("StartBundle"); }
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            log("ProcessElement");
+            process(c);
+        }
+
+        @FinishBundle
+        public void finishBundle() { this.log("FinishBundle"); }
+
+        @Teardown
+        public void teardown() { this.log("Teardown"); }
+
     }
 }
